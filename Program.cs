86b662706aad641309978208c3d9d252e2f30871
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:34:20 +0700

    Menambahkan class kodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class kodeProduk
+{
+    public static string getKodeProduk(string produk)
+    {
+        string[,] dataKodeProduk =
+        {
+            {"Laptop", "E100" },
+            {"Smartphone", "E101" },
+            {"Tablet", "E102" },
+            {"Headset", "E103" },
+            {"Keyboard", "E104" },
+            {"Mouse", "E105" },
+            {"Printer", "E106" },
+            {"Monitor", "E107" },
+            {"Smartwatch", "E108" },
+            {"kamera", "E109" }
+        };
+
+        for (int i = 0; i < dataKodeProduk.GetLength(0); i++)
+        {
+            if (dataKodeProduk[i, 0].Equals(produk, StringComparison.OrdinalIgnoreCase))
+            {
+                return dataKodeProduk[i, 1];
+            }
+        }
+        return "Kode produk tidak ditemukan";
+    }
+}
+
+//public class FunLaptop
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("1. Program kode Produk");
+        Console.WriteLine("2. Program edit Fan Laptop");
+        Console.WriteLine("0. Exit");
+        Console.WriteLine("Masukan pilihan: ");
+
+        String masuk = Console.ReadLine();
+
+        while(masuk != "0")
+        {
+            if (masuk == "1")
+            {
+                kodeProduk Kode = new kodeProduk();
+                Console.WriteLine("Masukan nama produk: ");
+                string produk = Console.ReadLine() ?? "";
+
+                if (produk != "kembali")
+                {
+                    string kode = kodeProduk.getKodeProduk(produk);
+                    Console.WriteLine($"Kode produk {produk} : {kode}");
+                }
+                else
+                {
+                    Main();
+                }
+            }else if (masuk == "0")
+            {
+                break;
+            }
+        }
+    }
+}
